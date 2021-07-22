1bf65d43dee36e590325e74955261e4e8eb6f4d0
Jayantie <Jayantie@DESKTOP-JAYA>
2021-07-21 22:39:45 -0300

    Belastingscalcuator clss aangemaakt

---
+++ belasting_calculator/src/belasting_calculator/main.java
@@ -0,0 +1,33 @@
+package belasting_calculator;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double belasting = 0;
+		double bruto = 0;
+		
+		//scanner object om data te invoeren
+		Scanner invoer = new Scanner(System.in);
+		
+		System.out.println("Voer uw bruto salaris in: ");
+	
+		//nextDoouble() geeft je de mogelijkheid om gebroken getallen (data tye = double) in te voeren	
+		//Alterntieven; nextInt(), nextLine
+		bruto = invoer.nextDouble();
+		
+		
+		if (bruto < 2000){
+			belasting = bruto * 0.10;	
+		}else {
+			belasting = bruto * 0.20;
+		}
+		
+		System.out.println("Uw belasting bedraagt "+ belasting);
+		
+		System.out.println("Netto salaris " + (bruto -belasting));
+	}
+}
